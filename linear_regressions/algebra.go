674479e6185b5fb04dc16bf53e13b77dced476f3
Tim de Klijn <timdeklijn@gmail.com>
2020-11-16 12:16:54 +0100

    Fixed naming issues

--- linear_regressions/algebra.go
+++ linear_regressions/algebra.go
@@ -9,66 +9,66 @@ type Arr []Vec
 // AddOnes creates an Array where each element is a list (Vec)
 // starting with 1.0.
 func (a Arr) AddOnes() Arr {
-	var new Arr
+	var arr Arr
 	for i := 0; i < len(a); i++ {
 		var tmp Vec
 		tmp = append(tmp, 1.0)
 		for _, ii := range a[i] {
 			tmp = append(tmp, ii)
 		}
-		new = append(new, tmp)
+		arr = append(arr, tmp)
 
 	}
-	return new
+	return arr
 }
 
 // AddOnes creates an Array where each element is a list (Vec)
 // starting with 1.0.
 func (v Vec) AddOnes() Arr {
-	var new Arr
+	var arr Arr
 	for _, aa := range v {
-		new = append(new, Vec{1.0, aa})
+		arr = append(arr, Vec{1.0, aa})
 	}
-	return new
+	return arr
 }
 
 // Transpose returns a transpose of the Array.
 func (a Arr) Transpose() Arr {
-	var new Arr
+	var arr Arr
 	for i := 0; i < len(a[0]); i++ {
 		var row Vec
-		new = append(new, row)
+		arr = append(arr, row)
 		for j := 0; j < len(a); j++ {
-			new[i] = append(new[i], a[j][i])
+			arr[i] = append(arr[i], a[j][i])
 		}
 	}
-	return new
+	return arr
 }
 
 // Transpose returns the transpose of the vector,
 // which is an Array.
 func (v Vec) Transpose() Arr {
-	var new Arr
+	var arr Arr
 	for _, i := range v {
-		new = append(new, Vec{i})
+		arr = append(arr, Vec{i})
 	}
-	return new
+	return arr
 }
 
 // Sub subtracts other from v. Returns a new Vec.
 func (v Vec) Sub(other Vec) Vec {
-	var new Vec
+	var vec Vec
 	for i := range v {
-		new = append(new, v[i]-other[i])
+		vec = append(vec, v[i]-other[i])
 	}
-	return new
+	return vec
 }
 
 // Add add other to v. Returns a new Vec.
 func (v Vec) Add(other Vec) Vec {
-	var new Vec
+	var vec Vec
 	for i := range v {
-		new = append(new, v[i]+other[i])
+		vec = append(vec, v[i]+other[i])
 	}
-	return new
+	return vec
 }
