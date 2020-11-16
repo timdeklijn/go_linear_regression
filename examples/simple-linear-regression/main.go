fd3d79e5b9940903fe5e5d8465f914dcee1565ae
Tim de Klijn <timdeklijn@gmail.com>
2020-11-16 15:26:49 +0100

    Clean up imports

--- examples/simple-linear-regression/main.go
+++ examples/simple-linear-regression/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"linear_regression/pkg/algebra"
+	. "linear_regression/pkg/algebra"
 	. "linear_regression/pkg/linear_regression"
 )
 
 func main() {
 	// Data
-	length := algebra.Vec{5, 5.5, 5.8, 6, 6.3, 6.5, 6.9, 7.1, 7.4, 7.7}
-	weight := algebra.Vec{7, 7.5, 7.3, 8, 7.7, 7.9, 8.2, 8.8, 8.4, 8.3}
+	length := Vec{5, 5.5, 5.8, 6, 6.3, 6.5, 6.9, 7.1, 7.4, 7.7}
+	weight := Vec{7, 7.5, 7.3, 8, 7.7, 7.9, 8.2, 8.8, 8.4, 8.3}
 
 	// Setup
 	x := length.AddOnes()
@@ -18,6 +18,6 @@ func main() {
 	lr.Fit()
 	fmt.Println("\nThetas:", lr.Thetas)
 
-	var sample = algebra.Arr{algebra.Vec{1, 10}}
+	var sample = Arr{Vec{1, 10}}
 	fmt.Println("Prediction:", lr.Predict(sample))
 }
